1762640e0e4b596b3d6ca8c310649ea5a3c48a22
christian.fritz <christian.fritz@1aa7bb90-9ff5-4f69-b167-f7964f9e8525>
2012-07-31 09:44:15 +0000

    verbose (does not help?)

--- src/soot/jimple/infoflow/util/ArgBuilder.java
+++ src/soot/jimple/infoflow/util/ArgBuilder.java
@@ -8,7 +8,7 @@ public class ArgBuilder {
 	 * @return
 	 */
 	public String[] buildArgs(String className){
-		String[] result = new String[8];
+		String[] result = new String[14];
 		result[0] = "-w";
 		result[1] = "-p";
 		result[2] = "cg.spark";
@@ -17,6 +17,12 @@ public class ArgBuilder {
 		result[5] = ".";
 		result[6] = "-pp";
 		result[7] = className;
+		result[8] = "-p";
+		result[9] = "jb";
+		result[10] = "use-original-names:true";
+		result[11] = "-p";
+		result[12] = "cg";
+		result[13] = "verbose:true";
 		
 		return result;
 	}

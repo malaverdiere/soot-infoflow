98deeefea447832ccb58f2c7f19680d2a9257267
christian.fritz <christian.fritz@1aa7bb90-9ff5-4f69-b167-f7964f9e8525>
2013-01-07 12:01:10 +0000

    argument added

--- src/soot/jimple/infoflow/util/BaseSelector.java
+++ src/soot/jimple/infoflow/util/BaseSelector.java
@@ -7,15 +7,15 @@ import soot.jimple.internal.JCastExpr;
 import soot.jimple.internal.JIfStmt;
 
 public class BaseSelector {
-	
-	public static Value selectBase(Value val){
+	//we want to keep ArrayRef for objects on the right side of the assignment
+	public static Value selectBase(Value val, boolean right){
 		//we taint base of array instead of array elements
-		if (val instanceof ArrayRef) {
-			return selectBase(((ArrayRef) val).getBase());
+		if (val instanceof ArrayRef && !right) {
+			return selectBase(((ArrayRef) val).getBase(), right);
 		}
 		
 		if (val instanceof JCastExpr) {
-			return selectBase((((JCastExpr) val).getOpBox().getValue()));
+			return selectBase((((JCastExpr) val).getOpBox().getValue()), right);
 		}
 		
 		return val;

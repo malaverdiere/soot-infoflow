63fa119c743123d30adeef1523e523e6312bf85a
christian.fritz <christian.fritz@1aa7bb90-9ff5-4f69-b167-f7964f9e8525>
2013-01-19 14:49:45 +0000

    added Heaptestcode

---
+++ test/soot/jimple/infoflow/test/junit/HeapTests.java
@@ -0,0 +1,21 @@
+package soot.jimple.infoflow.test.junit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import soot.jimple.infoflow.Infoflow;
+
+public class HeapTests extends JUnitTests {
+
+	  @Test
+	    public void heapTest1(){
+		  Infoflow infoflow = initInfoflow();
+	    	List<String> epoints = new ArrayList<String>();
+	    	epoints.add("<soot.jimple.infoflow.test.HeapTestCode: void methodTest1()>");
+			infoflow.computeInfoflow(path, epoints,sources, sinks);
+			checkInfoflow(infoflow);
+	    }
+	  
+}

57ac7f3a4aa83896753b15d4e209e0c3e46c2221
cfritz <christian@fritz-online.org>
2013-04-03 09:02:44 +0200

    added missing classes

---
+++ test/soot/jimple/infoflow/test/securibench/supportClasses/DummyServletInputStream.java
@@ -0,0 +1,16 @@
+package soot.jimple.infoflow.test.securibench.supportClasses;
+
+import java.io.IOException;
+
+import javax.servlet.ServletInputStream;
+
+public class DummyServletInputStream extends ServletInputStream {
+
+	@Override
+	public int read() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}

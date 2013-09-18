ef382fad720c882ba9d19e620722855d4b8092c5
Steven Arzt <Steven.Arzt@cased.de>
2013-09-18 17:35:27 +0200

    added some Javadoc

--- src/soot/jimple/infoflow/heros/SolverCallToReturnFlowFunction.java
+++ src/soot/jimple/infoflow/heros/SolverCallToReturnFlowFunction.java
@@ -6,6 +6,13 @@ import java.util.Set;
 
 import soot.jimple.infoflow.data.Abstraction;
 
+/**
+ * A special implementation of the call-to-return flow function that allows
+ * access to the fact associated with the method's start point (i.e. the
+ * current context).
+ *  
+ * @author Steven Arzt
+ */
 public abstract class SolverCallToReturnFlowFunction implements FlowFunction<Abstraction> {
 
 	@Override
@@ -13,6 +20,13 @@ public abstract class SolverCallToReturnFlowFunction implements FlowFunction<Abs
 		return computeTargets(null, source);
 	}
 
+	/**
+	 * Computes the abstractions at the return site
+	 * @param d1 The abstraction at the beginning of the caller, i.e. the
+	 * context in which the method call is made
+	 * @param d2 The abstraction at the call site
+	 * @return The set of abstractions at the first node inside the callee
+	 */
 	public abstract Set<Abstraction> computeTargets(Abstraction d1, Abstraction d2);
 	
 }
--- src/soot/jimple/infoflow/heros/SolverReturnFlowFunction.java
+++ src/soot/jimple/infoflow/heros/SolverReturnFlowFunction.java
@@ -6,6 +6,13 @@ import java.util.Set;
 import soot.jimple.infoflow.data.Abstraction;
 import heros.FlowFunction;
 
+/**
+ * A special implementation of the return flow function that allows access to
+ * the facts associated with the start points of the callees (i.e. the contexts
+ * to which we return).
+ *  
+ * @author Steven Arzt
+ */
 public abstract class SolverReturnFlowFunction implements FlowFunction<Abstraction> {
 
 	@Override
@@ -13,6 +20,14 @@ public abstract class SolverReturnFlowFunction implements FlowFunction<Abstracti
 		return computeTargets(source, Collections.<Abstraction>emptySet());
 	}
 
+	/**
+	 * Computes the abstractions at the return site.
+	 * @param source The abstraction at the exit node
+	 * @param callerD1s The abstractions at the start nodes of all methods to
+	 * which we return (i.e. the contexts to which this flow function will be
+	 * applied).
+	 * @return The set of abstractions at the return site.
+	 */
 	public abstract Set<Abstraction> computeTargets(Abstraction source, Set<Abstraction> callerD1s);
 	
 }
